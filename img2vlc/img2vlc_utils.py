e6957f9d6c5660ad2e880f6a2e7909972feaabf2
DPChanny <kyoungchan0516@gmail.com>
2024-06-25 21:46:39 +0900

    reordered functions

--- img2vlc/img2vlc_utils.py
+++ img2vlc/img2vlc_utils.py
@@ -5,10 +5,10 @@ from torchvision.models.detection import (
     FasterRCNN_ResNet50_FPN_Weights, FasterRCNN_ResNet50_FPN_V2_Weights,
     fasterrcnn_resnet50_fpn_v2, fasterrcnn_resnet50_fpn, faster_rcnn)
 
-from utils import get_vlc_map
 from img2vlc.CoordConv2d import CoordConv2d
 from img2vlc.img2vlc_configs import (ModelVersion, CoordConv2dVersion,
                                      MODEL_PATH, COORD_CONV_2D_VERSION, IMG2VLC_VERSION)
+from utils import get_vlc_map
 
 
 def collate_fn(batch):
@@ -19,6 +19,48 @@ def collate_fn(batch):
         target_list.append(target)
 
     return image_list, target_list
+
+
+def load_checkpoint(device):
+    if os.path.exists(os.path.join(MODEL_PATH,
+                                   str(IMG2VLC_VERSION),
+                                   str(COORD_CONV_2D_VERSION),
+                                   "checkpoint.pth")):
+        checkpoint = torch.load(os.path.join(MODEL_PATH,
+                                             str(IMG2VLC_VERSION),
+                                             str(COORD_CONV_2D_VERSION),
+                                             "checkpoint.pth"),
+                                map_location=device)
+    else:
+        checkpoint = None
+
+    return checkpoint
+
+
+def save_checkpoint(epoch, total_epoch, batch, total_batch, model, optimizer, scheduler):
+    if not os.path.exists(os.path.join(MODEL_PATH,
+                                       str(IMG2VLC_VERSION),
+                                       str(COORD_CONV_2D_VERSION))):
+        os.makedirs(os.path.join(MODEL_PATH,
+                                 str(IMG2VLC_VERSION),
+                                 str(COORD_CONV_2D_VERSION)))
+
+    values = {'start_epoch': epoch,
+              'start_batch': batch,
+              'model': model.state_dict(),
+              'optimizer': optimizer.state_dict(),
+              'scheduler': scheduler.state_dict()}
+
+    torch.save(values, os.path.join(MODEL_PATH,
+                                    str(IMG2VLC_VERSION),
+                                    str(COORD_CONV_2D_VERSION),
+                                    "{}-{}_{}-{}.pth".format(epoch, total_epoch,
+                                                             batch, total_batch)))
+    torch.save(values, os.path.join(MODEL_PATH,
+                                    str(IMG2VLC_VERSION),
+                                    str(COORD_CONV_2D_VERSION),
+                                    "checkpoint.pth".format(epoch, total_epoch,
+                                                            batch, total_batch)))
 
 
 def get_model(model_version, coord_conv_2d_version, device, log_model=True):
@@ -47,20 +89,11 @@ def get_model(model_version, coord_conv_2d_version, device, log_model=True):
     return model
 
 
-def load_checkpoint(device):
-    if os.path.exists(os.path.join(MODEL_PATH,
-                                   str(IMG2VLC_VERSION),
-                                   str(COORD_CONV_2D_VERSION),
-                                   "checkpoint.pth")):
-        checkpoint = torch.load(os.path.join(MODEL_PATH,
-                                             str(IMG2VLC_VERSION),
-                                             str(COORD_CONV_2D_VERSION),
-                                             "checkpoint.pth"),
-                                map_location=device)
-    else:
-        checkpoint = None
+def load_model(model, checkpoint):
+    if checkpoint is not None:
+        model.load_state_dict(checkpoint['model'])
 
-    return checkpoint
+    return model
 
 
 def load_optimizer(optimizer, checkpoint):
@@ -81,41 +114,8 @@ def load_starts(checkpoint):
     return start_epoch, start_batch
 
 
-def load_model(model, checkpoint):
-    if checkpoint is not None:
-        model.load_state_dict(checkpoint['model'])
-
-    return model
-
-
 def load_scheduler(scheduler, checkpoint):
     if checkpoint is not None:
         scheduler.load_state_dict(checkpoint['scheduler'])
 
     return scheduler
-
-
-def save_checkpoint(epoch, total_epoch, batch, total_batch, model, optimizer, scheduler):
-    if not os.path.exists(os.path.join(MODEL_PATH,
-                                       str(IMG2VLC_VERSION),
-                                       str(COORD_CONV_2D_VERSION))):
-        os.makedirs(os.path.join(MODEL_PATH,
-                                 str(IMG2VLC_VERSION),
-                                 str(COORD_CONV_2D_VERSION)))
-
-    values = {'start_epoch': epoch,
-              'start_batch': batch,
-              'model': model.state_dict(),
-              'optimizer': optimizer.state_dict(),
-              'scheduler': scheduler.state_dict()}
-
-    torch.save(values, os.path.join(MODEL_PATH,
-                                    str(IMG2VLC_VERSION),
-                                    str(COORD_CONV_2D_VERSION),
-                                    "{}-{}_{}-{}.pth".format(epoch, total_epoch,
-                                                             batch, total_batch)))
-    torch.save(values, os.path.join(MODEL_PATH,
-                                    str(IMG2VLC_VERSION),
-                                    str(COORD_CONV_2D_VERSION),
-                                    "checkpoint.pth".format(epoch, total_epoch,
-                                                            batch, total_batch)))
