523eb690e30a2c45f7fc7cef118ace5ab3796ef0
kyoung chan <kyoungchan0516@gmail.com>
2024-06-27 20:21:23 +0900

    analyze complete

--- preprocessing/analyze.py
+++ preprocessing/analyze.py
@@ -1,10 +1,11 @@
-import os
 import json
+import os
 
 from PIL import Image
+from matplotlib.gridspec import GridSpec
 
 from configs import JSON_PATH, DATA_PATH, JPG_PATH
-from utils import json_parser
+from utils import json_parser, get_vlc2tok
 
 import matplotlib.pyplot as plt
 
@@ -17,17 +18,17 @@ result = {'vlc_count': {},
           'vlc_lens': [],
           'flc_lens': [],
           'widths': [],
-          'heights': []}
+          'heights': [],
+          'sizes': []}
 
 if from_start:
-
-    for index, json in enumerate(json_list):
+    for index, json_path in enumerate(json_list):
         if not index % 1000:
             print(index)
-        data_name = os.path.splitext(json)[0]
+        data_name = os.path.splitext(json_path)[0]
         flcs, vlcs, boxes = json_parser(os.path.join(DATA_PATH,
                                                      JSON_PATH,
-                                                     json))
+                                                     json_path))
 
         result['vlc_lens'].append(len(vlcs))
         result['flc_lens'].append(len(flcs))
@@ -39,13 +40,9 @@ if from_start:
         image = Image.open(os.path.join(DATA_PATH, JPG_PATH, data_name + ".jpg")).convert("RGB")
         result['widths'].append(image.width)
         result['heights'].append(image.height)
-        image.close()
+        result['sizes'].append(image.width * image.height)
 
-    print(max(result['flc_lens']), min(result['flc_lens']), sum(result['flc_lens']) / len(result['flc_lens']))
-    print(max(result['vlc_lens']), min(result['vlc_lens']), sum(result['vlc_lens']) / len(result['vlc_lens']))
-    print(max(result['widths']), min(result['widths']), sum(result['widths']) / len(result['widths']))
-    print(max(result['heights']), min(result['heights']), sum(result['heights']) / len(result['heights']))
-    print(result['vlc_count'])
+        image.close()
 
     with open("result.json", "w") as f:
         json.dump(result, f)
@@ -53,20 +50,34 @@ if from_start:
 with open("result.json", "r") as f:
     result = json.load(f)
 
-plt.bar(result['vlc_count'].keys(), result['vlc_count'].values())
-plt.xticks(result['vlc_count'].keys(), result['vlc_count'].values())
-plt.imsave("result.png")
+fig = plt.figure(figsize=(40, 20))
+gs = GridSpec(nrows=2, ncols=3)
 
+for vlc in get_vlc2tok().keys():
+    if vlc not in result['vlc_count']:
+        result['vlc_count'][vlc] = 0
+result['vlc_count'] = {k: v for (k, v) in sorted(result['vlc_count'].items(), key=lambda x: x[1], reverse=True)}
 
-# 115 7 34.30829
-# 64 5 18.56227
-# 5340 259 1401.68117
-# 1197 100 376.78104
+ax1 = fig.add_subplot(gs[0, :])
+ax1.set_title('vlc count')
+ax1.bar(result['vlc_count'].keys(), result['vlc_count'].values())
+ax1.set_xticks(range(len(result['vlc_count'].keys())))
+ax1.set_xticklabels(result['vlc_count'].keys(), rotation=90)
 
-# {'\\lim_': 111223, 'v': 15460, '\\to': 111223, '3': 62734, '\\frac': 140649, 'd': 44148, '\\left(': 54150,
-# 'e': 6420, '+': 113617, '-': 95911, '2': 91587, '\\sin': 39494, '9': 42619, '\\right)': 54150, '1': 35085,
-# '8': 41951, '4': 42610, 'c': 15571, '\\pi': 43990, '\\cos': 28729, '\\tan': 34033, '=': 9801, 't': 26317,
-# '/': 25945, '\\sec': 11581, 'x': 63050, '7': 41773, '0': 19159, '\\cot': 3080, 'h': 31189, '\\ln': 10980,
-# 'w': 30188, '\\infty': 21187, 'k': 15634, '6': 41671, 'y': 29048, '5': 41726, 'u': 30337, '\\sqrt': 12140,
-# 's': 14038, '\\log': 15222, 'z': 16233, '\\cdot': 1264, '\\csc': 6827, '\\left|': 4407, '\\right|': 4407,
-# 'p': 15474, 'b': 15527, 'n': 11906, 'g': 15613, 'a': 15615, '\\theta': 14726, 'r': 14753, '.': 55}
+ax2 = fig.add_subplot(gs[1, 0])
+ax2.set_title('vlc lens')
+ax2.plot(result['vlc_lens'], linewidth=0.01)
+ax2.hlines(sum(result['vlc_lens']) / len(result['vlc_lens']), 0, len(result['vlc_lens']) - 1, color='red')
+
+ax3 = fig.add_subplot(gs[1, 1])
+ax3.set_title('flc lens')
+ax3.plot(result['flc_lens'], linewidth=0.01)
+ax3.hlines(sum(result['flc_lens']) / len(result['flc_lens']), 0, len(result['flc_lens']) - 1, color='red')
+
+ax4 = fig.add_subplot(gs[1, 2])
+ax4.set_title('sizes')
+ax4.plot(result['sizes'], linewidth=0.01)
+ax4.hlines(sum(result['sizes']) / len(result['sizes']), 0, len(result['sizes']) - 1, color='red')
+
+fig.show()
+fig.savefig("result.png")
